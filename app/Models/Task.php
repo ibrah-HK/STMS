44697dad0fac29a9ce139cec54af466338176b29
ibrah-HK <rabihmi100@gmail.com>
2026-01-06 12:52:30 +0300

    task & migration with relations

---
+++ app/Models/Task.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = [
+        'category_id',
+        'title',
+        'description',
+        'status',
+    ];
+}
---
+++ database/migrations/2026_01_06_093600_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Ramsey\Uuid\Type\Integer;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->Integer('category_id');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('status', ['pending', 'completed', ]);
+            $table->timestamps();
+
+            //insering foreign key constraints
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
