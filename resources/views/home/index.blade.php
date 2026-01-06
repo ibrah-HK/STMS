00051477909841115fa18c772e23872be38d413a
ibrah-HK <rabihmi100@gmail.com>
2026-01-06 10:04:39 +0300

    controller-route-view for homepage

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Home Page')
+
+@section('content')
+    <h3>Welcome to the Home Page</h3>
+    <p>This is the content of the home page.</p>
+@endsection
+php 
